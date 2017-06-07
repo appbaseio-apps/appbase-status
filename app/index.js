1a68316cc61a25366df3ad9da400d47d704224b9
shahsaurabh0605 <shahsaurabh0103@gmail.com>
2017-06-07 15:25:43 +0530

    react setup

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Main extends Component {
+	render() {
+		return (
+			<div>
+                dysdvjvsd
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById("app"));
