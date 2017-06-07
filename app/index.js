2ac12646f5aa260b0000aad3bdca2bea4d5d41cf
devAbnull <abhisandhyasp.ap@gmail.com>
2017-06-07 15:56:34 +0530

    react-http-requet added

--- app/index.js
+++ app/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-class Main extends Component {
-	render() {
-		return (
-			<div>
-                dysdvjvsd
-			</div>
-		);
-	}
+import Request from 'react-http-request';
+ 
+export default class App extends Component {
+  render() {
+    return (
+      <Request
+        url='https://slack-redir.net/link?url=https%3A%2F%2Fappbase-apifrontend-funtests.s3.amazonaws.com%2FFunctionalTestResult_es2.txt'
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else {
+              return <div>{ JSON.stringify(result) }</div>;
+            }
+          }
+        }
+      </Request>
+    );
+  }
 }
 
-ReactDOM.render(<Main />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
